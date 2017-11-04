ac1dc76fbece659f65f0bd57622497f524725258
Aleksandar <vukasinovic@outlook.com>
2017-11-04 21:20:02 +0100

    register login

--- public/angular/templates/register/register.ctrl.js
+++ public/angular/templates/register/register.ctrl.js
@@ -14,7 +14,14 @@ app.controller('RegisterCtrl', function ($scope, $http, ROUTES, Notification, $l
                 $cookies.put('user', JSON.stringify(response.data.data.user));
                 $location.path('/');
             }, function (error) {
-                Notification.error({message: error.data.message, positionY: 'bottom', positionX: 'right', delay: 1000});
+                if(error.data.code === 422) {
+                    angular.forEach(error.data.message, function (value, key) {
+                        Notification.error({message: key + ': ' + value, positionY: 'bottom', positionX: 'right', delay: 1000});
+                    });
+                } else {
+                    Notification.error({message: error.data.message, positionY: 'bottom', positionX: 'right', delay: 1000});
+                }
+
             });
     };
 });
